043ddb1c88345d6318bcb28e8ae82358fd3b21ca
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-18 00:55:53 +0600

    Создал класс для удобной отправки сообщений, просит написат Тему и Собщение, отправляет только моему телефону!)

---
+++ PushNat/MobileMessagingClient.cs
@@ -0,0 +1,38 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System.Threading.Tasks;
+
+namespace PushNotification
+{
+    public class MobileMessagingClient
+    {
+        private readonly FirebaseMessaging messaging;
+
+        public MobileMessagingClient()
+        {
+            var app = FirebaseApp.Create(new AppOptions() { Credential = GoogleCredential.FromFile("fir-app-659c6-firebase-adminsdk-sjxdp-6e5e08c5d4.json").CreateScoped("https://www.googleapis.com/auth/firebase.messaging") });
+            messaging = FirebaseMessaging.GetMessaging(app);
+            messaging = FirebaseMessaging.GetMessaging(app);
+        }
+
+        private Message CreateNotification(string title, string notificationBody, string token)
+        {
+            return new Message()
+            {
+                Token = token,
+                Notification = new Notification()
+                {
+                    Body = notificationBody,
+                    Title = title
+                }
+            };
+        }
+
+        public async Task SendNotification(string token, string title, string body)
+        {
+            var result = await messaging.SendAsync(CreateNotification(title, body, token));
+            //do something with result
+        }
+    }
+}
---
+++ PushNat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PushNotification
+{
+    // Nuget имя пакета firebaseAdmin
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MobileMessagingClient client = new MobileMessagingClient();
+            Console.WriteLine("Topic: ");
+            var tokenId = "cxSJw4R5jjs:APA91bGh6l9ahNi2MTGraxyNntWCIn7nHvpVtZRG9p9UTp8Aq2OhZg38WQ3FZWGrlkeoPV9u3xj7wQMOCOvwSmsDaDp0D4sbHcAOxYMlINtKkGyJbBQC0tHmNxzZf_S1wocI9ENmEGBA";
+            var topic = Console.ReadLine();
+            Console.WriteLine("Notification message: ");
+            var notification = Console.ReadLine();
+            client.SendNotification(tokenId, topic, notification).Wait();
+
+
+
+        }
+    }
+}
